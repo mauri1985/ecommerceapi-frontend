1ef78aeb9db576b0124be79a2730b1095b8a4221
Mauricio <mghb1985@gmail.com>
2026-09-06 20:34:34 -0300

    Se cambia numero de whatsapp por uno falso

--- src/data/InfoContacto.jsx
+++ src/data/InfoContacto.jsx
@@ -1,6 +1,6 @@
 export const INFO_CONTACTO = {
   telefono: "+598 99 123 456",
-  whatsapp: "59899123456",
+  whatsapp: "59800000000",
   email: "contacto@mitienda.com",
   horario: "Lunes a viernes de 9:00 a 18:00hs",
   domicilio: "Av. 18 de Julio 1234, Montevideo",
