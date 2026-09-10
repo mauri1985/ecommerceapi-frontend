cd1e962a905975f47b3d5af8f310b756ac1ce58a
Mauricio <30642578+mauri1985@users.noreply.github.com>
2026-09-10 19:45:37 -0300

    Update contact information in InfoContacto.jsx

--- src/data/InfoContacto.jsx
+++ src/data/InfoContacto.jsx
@@ -1,7 +1,7 @@
 export const INFO_CONTACTO = {
-  telefono: "+598 99 123 456",
-  whatsapp: "59800000000",
+  telefono: "+598 95291547",
+  whatsapp: "59895291547",
   email: "contacto@mitienda.com",
   horario: "Lunes a viernes de 9:00 a 18:00hs",
-  domicilio: "Av. 18 de Julio 1234, Montevideo",
+  domicilio: "Av. Solano Lopez 1781",
 };
