cc805b4bc917fbd56cfa7c1467a726570f659ec9
Mauricio <mghb1985@gmail.com>
2026-09-12 12:08:36 -0300

    Ajustes InfoContacto

--- src/data/InfoContacto.jsx
+++ src/data/InfoContacto.jsx
@@ -1,11 +1,7 @@
 export const INFO_CONTACTO = {
   telefono: "+598 95291547",
   whatsapp: "59895291547",
-<<<<<<< HEAD
-  email: "contacto@aurashop.com",
-=======
-  email: "contacto@mitienda.com",
->>>>>>> cd1e962a905975f47b3d5af8f310b756ac1ce58a
+  email: "contacto@maurishop.com",
   horario: "Lunes a viernes de 9:00 a 18:00hs",
   domicilio: "Av. Solano Lopez 1781",
 };
