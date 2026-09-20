34b722196039d295215e17cc355f6aca6f3c36f3
Mauricio <mghb1985@gmail.com>
2026-09-20 12:29:13 -0300

    Mismo borde buscador mogile y botones de ordenar

--- src/components/OrdenPrecio.jsx
+++ src/components/OrdenPrecio.jsx
@@ -7,10 +7,10 @@ export default function OrdenPrecio({ orden, onCambiarOrden }) {
         onClick={() =>
           onCambiarOrden(orden === "precio_asc" ? "" : "precio_asc")
         }
-        className={`flex flex-row h-10 md:h-9 rounded-md border  w-1/2 cursor-pointer items-center justify-center ${
+        className={`flex flex-row h-10 md:h-9 rounded-md ring ring-slate-400  w-1/2 cursor-pointer items-center justify-center ${
           orden === "precio_asc"
             ? "bg-blue-600 border-blue-600 text-white"
-            : "bg-white border-gray-400 hover:bg-gray-200 text-gray-600"
+            : "bg-white border-slate-400 hover:bg-slate-200 text-slate-600"
         }`}
       >
         <p className="w-full text-center">Precio Menor</p>
@@ -19,10 +19,10 @@ export default function OrdenPrecio({ orden, onCambiarOrden }) {
         onClick={() =>
           onCambiarOrden(orden === "precio_desc" ? "" : "precio_desc")
         }
-        className={`flex flex-row h-10 md:h-9 rounded-md border w-1/2 cursor-pointer items-center justify-center ${
+        className={`flex flex-row h-10 md:h-9 rounded-md ring-1 ring-slate-400 w-1/2 cursor-pointer items-center justify-center ${
           orden === "precio_desc"
             ? "bg-blue-600 border-blue-600 text-white"
-            : "bg-white border-gray-400 hover:bg-gray-100 text-gray-600"
+            : "bg-white border-slate-400 hover:bg-slate-100 text-slate-600"
         }`}
       >
         <p className="w-full text-center ">Precio Mayor</p>
