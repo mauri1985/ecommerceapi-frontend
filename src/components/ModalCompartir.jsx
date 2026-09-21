29f435b69e8e3e7d287ede1bd4ec446fcc7f3212
Mauricio <mghb1985@gmail.com>
2026-09-21 08:49:27 -0300

    Se resuelve error en ModalCompartir

--- src/components/ModalCompartir.jsx
+++ src/components/ModalCompartir.jsx
@@ -1,4 +1,4 @@
-import { X, MessageCircle, Facebook, Mail, Link2, Check } from "lucide-react";
+import { X, MessageCircle, Mail, Link2, Check } from "lucide-react";
 import { useState } from "react";
 import { useToast } from "../context/ToastContext";
 
@@ -26,14 +26,28 @@ export default function ModalCompartir({
     },
     {
       nombre: "Facebook",
-      icono: Facebook,
+      icono: () => (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="w-5 h-5"
+        >
+          <path d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.95c5.05-.5 9-4.76 9-9.95z" />
+        </svg>
+      ),
       color: "bg-blue-600 hover:bg-blue-700",
       href: `https://www.facebook.com/sharer/sharer.php?u=${urlCodificada}`,
     },
     {
       nombre: "X (Twitter)",
       icono: () => (
-        <svg viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="w-5 h-5"
+        >
           <path d="M18.9 1.153h3.68l-8.04 9.19L23.6 22.846h-7.406l-5.8-7.584-6.638 7.584H.07l8.6-9.83L.4 1.153H7.98l5.24 6.932zM17.61 20.65h2.04L6.486 3.24H4.298z" />
         </svg>
       ),
